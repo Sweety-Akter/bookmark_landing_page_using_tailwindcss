720a1bfaf5d71192a9dec8cece28033c054071b5
sweety <sweety469682@gmail.com>
2021-07-16 17:24:18 +0600

    upload full proj

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "bookmark-purple": "#5267df", 
+        "bookmark-red": "#FA5959", 
+        "bookmark-blue": "#242A45", 
+        "bookmark-grey": "#9194A2", 
+        "bookmark-white": "#f7f7f7", 
+      }
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
